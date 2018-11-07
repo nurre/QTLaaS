ac2ee02be8d0353ead246298d8a7b2715f2080eb
nurre <nurre13@gmail.com>
2018-11-07 18:05:58 +0000

    quickfix

--- qtlaas_server.py
+++ qtlaas_server.py
@@ -82,4 +82,4 @@ def uploaded_file(filename):
 
 if __name__ == '__main__':
 	#app.run(debug=True) #For Internal
-	app.run(host=0.0.0.0, debug=True) #For external
+	app.run(host="0.0.0.0", debug=True) #For external

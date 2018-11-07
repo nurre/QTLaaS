83634270764655c312a3e7b9e6308679c4e144cc
NevineMGouda <Nevine.Gouda.9578@student.uu.se>
2018-11-07 18:04:15 +0100

    Remove unnecessary commands

--- run_linux_cmds.py
+++ run_linux_cmds.py
@@ -7,16 +7,21 @@ logger = logging.getLogger(__name__)
 
 
 def run_linux_cmds():
-    system("eval `ssh-agent -s`")
-    system("ssh-add group12.pem")
-    f = open("linux_commands.txt","r")
+    try:
+        f = open("linux_commands.txt", "r")
+
+    except:
+        logger.error("__ACC__:Something went wrong while attempting to open 'linux_commands.txt' file. "
+                     "Make sure to run qtlaas_automation.find_new_workers() first and try again.")
+        return False
+
     linux_cmds = f.readlines()
     for line in linux_cmds:
         command = line.strip()
         try:
             system(command)
         except:
-            logger.error("__ACC__:Something went wrong while attempting to run: " + linux_cmds + "Skipping this instance")
+            logger.error("__ACC__:Something went wrong while attempting to run: " + line + "Skipping this instance")
             logger.error('__ACC__: Try to run the command manually.')
             continue
 run_linux_cmds()

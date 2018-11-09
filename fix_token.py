37b585ae8904a0a55c4711a9d6b4e6de167f6fe0
NevineMGouda <Nevine.Gouda.9578@student.uu.se>
2018-11-09 18:45:32 +0100

    Write jupyter token to a token file

--- fix_token.py
+++ fix_token.py
@@ -1,14 +1,15 @@
 def fix_token_file():
-    f = open("token.txt","r")
+    filename = "token.txt"
+    f = open(filename,"r")
     lines = f.readlines()
-    token=""
+    token = ""
     for line in lines:
         if "?token=" not in line:
             continue
         i = line.find("?token=")
         token = line[i + len("?token="):].split()[0]+"\n"
         break
-    f = open("token.txt", "w")
+    f = open(filename, "w")
     f.write(token)
     f.close()
 
